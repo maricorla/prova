fd58126ef56b94f8c2daf5eaedb61398cf8b0c35
marina <marinagulevichqa@gmail.com>
2019-11-20 13:20:25 +0100

    added action

--- app/src/main/java/com/example/myprovaapp/MainActivity.kt
+++ app/src/main/java/com/example/myprovaapp/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.myprovaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            getNickName(it)
+        }
+
     }
+
+    private fun getNickName(view:View) {
+        val takeName = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = takeName.text
+        takeName.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+    }
+
 }
